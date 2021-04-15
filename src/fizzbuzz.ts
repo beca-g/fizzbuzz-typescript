77d362ff4cff10809f25d90c07b97345ac9c490f
Beca-g <rebekahgalliano@gmail.com>
2021-04-15 12:09:02 +0100

    Removed unnecessary number type from functions

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -4,15 +4,15 @@ export class FizzBuzz {
     return (number % divisor === 0)
   }
 
-  divisibleByThree(number: number):number | string | void {
+  divisibleByThree(number: number): string | void {
     if(this.divisibleBy(number, 3)) return "Fizz"
   }
 
-  divisibleByFive(number: number):number | string | void {
+  divisibleByFive(number: number): string | void {
     if(this.divisibleBy(number, 5)) return "Buzz"
   }
 
-  divisibleByThreeAndFive(number: number): number | string | void {
+  divisibleByThreeAndFive(number: number): string | void {
     if(this.divisibleBy(number, 15)) return "FizzBuzz"
   }
 }
